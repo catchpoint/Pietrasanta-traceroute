fc8b63e6658e4b5ece2b19c1be7241e0b4b7da82
Luca Sani <lsani@catchpoint.com>
2023-07-26 09:18:34 -0400

    style

--- traceroute/mod-tcp.c
+++ traceroute/mod-tcp.c
@@ -1,6 +1,6 @@
 /*
-    Copyright (c)  2006, 2007		Dmitry Butskoy
-					<buc@citadel.stu.neva.ru>
+    Copyright (c)  2006, 2007        Dmitry Butskoy
+                    <buc@citadel.stu.neva.ru>
     License:  GPL v2 or any later
 
     See COPYING for the status of this software.
@@ -23,7 +23,7 @@
 
 
 #ifndef IP_MTU
-#define IP_MTU	14
+#define IP_MTU    14
 #endif
 
 
@@ -36,47 +36,47 @@ static int last_ttl = 0;
 
 static unsigned pseudo_IP_header_size = 0;
 static uint8_t* buf;
-static uint8_t tmp_buf[1024];	    /*  enough, enough...  */
+static uint8_t tmp_buf[1024];        /*  enough, enough...  */
 static size_t* length_p;
 static sockaddr_any src;
 static struct tcphdr *th = NULL;
 
-#define TH_FLAGS(TH)	(((uint8_t *) (TH))[13])
-#define TH_FIN	0x01
-#define TH_SYN	0x02
-#define TH_RST	0x04
-#define TH_PSH	0x08
-#define TH_ACK	0x10
-#define TH_URG	0x20
-#define TH_ECE	0x40
-#define TH_CWR	0x80
+#define TH_FLAGS(TH)    (((uint8_t *) (TH))[13])
+#define TH_FIN    0x01
+#define TH_SYN    0x02
+#define TH_RST    0x04
+#define TH_PSH    0x08
+#define TH_ACK    0x10
+#define TH_URG    0x20
+#define TH_ECE    0x40
+#define TH_CWR    0x80
 
 
-static int flags = 0;	    /*  & 0xff == tcp_flags ...  */
+static int flags = 0;        /*  & 0xff == tcp_flags ...  */
 static int sysctl = 0;
 static int reuse = 0;
 static unsigned int mss = 0;
 static int info = 0;
 
-#define FL_FLAGS	0x0100
-#define FL_ECN		0x0200
-#define FL_SACK		0x0400
-#define FL_TSTAMP	0x0800
-#define FL_WSCALE	0x1000
+#define FL_FLAGS    0x0100
+#define FL_ECN        0x0200
+#define FL_SACK        0x0400
+#define FL_TSTAMP    0x0800
+#define FL_WSCALE    0x1000
 
 
 static struct {
-	const char *name;
-	unsigned int flag;
+    const char *name;
+    unsigned int flag;
 } tcp_flags[] = {
-	{ "fin", TH_FIN },
-	{ "syn", TH_SYN },
-	{ "rst", TH_RST },
-	{ "psh", TH_PSH },
-	{ "ack", TH_ACK },
-	{ "urg", TH_URG },
-	{ "ece", TH_ECE },
-	{ "cwr", TH_CWR },
+    { "fin", TH_FIN },
+    { "syn", TH_SYN },
+    { "rst", TH_RST },
+    { "psh", TH_PSH },
+    { "ack", TH_ACK },
+    { "urg", TH_URG },
+    { "ece", TH_ECE },
+    { "cwr", TH_CWR },
 };
 
 static char* names_by_flags(unsigned int flags)
@@ -159,177 +159,177 @@ static CLIF_option tcp_options[] = {
 
 static int tcp_init(const sockaddr_any* dest, unsigned int port_seq, size_t* packet_len_p)
 {
-	int af = dest->sa.sa_family;
-	int mtu;
-	socklen_t len;
-	uint8_t* ptr;
-	uint16_t* lenp;
+    int af = dest->sa.sa_family;
+    int mtu;
+    socklen_t len;
+    uint8_t* ptr;
+    uint16_t* lenp;
 
-	dest_addr = *dest;
-	dest_addr.sin.sin_port = 0;	/*  raw sockets can be confused   */
+    dest_addr = *dest;
+    dest_addr.sin.sin_port = 0;    /*  raw sockets can be confused   */
 
-	if(!port_seq)  port_seq = DEF_TCP_PORT;
-	dest_port = htons (port_seq);
-
-
-	/*  Create raw socket for tcp   */
-
-	raw_sk = socket(af, SOCK_RAW, IPPROTO_TCP);
-	if(raw_sk < 0)
-		error_or_perm ("socket");
-
-	tune_socket(raw_sk);	    /*  including bind, if any   */
-
-	if(connect(raw_sk, &dest_addr.sa, sizeof(dest_addr)) < 0)
-		error ("connect");
-
-	len = sizeof(src);
-	if(getsockname(raw_sk, &src.sa, &len) < 0)
-		error ("getsockname");
+    if(!port_seq)  port_seq = DEF_TCP_PORT;
+    dest_port = htons (port_seq);
 
 
-	len = sizeof(mtu);
-	if(getsockopt(raw_sk, af == AF_INET ? SOL_IP : SOL_IPV6, af == AF_INET ? IP_MTU : IPV6_MTU, &mtu, &len) < 0 || mtu < 576)
+    /*  Create raw socket for tcp   */
+
+    raw_sk = socket(af, SOCK_RAW, IPPROTO_TCP);
+    if(raw_sk < 0)
+        error_or_perm ("socket");
+
+    tune_socket(raw_sk);        /*  including bind, if any   */
+
+    if(connect(raw_sk, &dest_addr.sa, sizeof(dest_addr)) < 0)
+        error ("connect");
+
+    len = sizeof(src);
+    if(getsockname(raw_sk, &src.sa, &len) < 0)
+        error ("getsockname");
+
+
+    len = sizeof(mtu);
+    if(getsockopt(raw_sk, af == AF_INET ? SOL_IP : SOL_IPV6, af == AF_INET ? IP_MTU : IPV6_MTU, &mtu, &len) < 0 || mtu < 576)
         mtu = 576;
 
-	/*  mss = mtu - headers   */
-	mtu -= af == AF_INET ? sizeof(struct iphdr) : sizeof(struct ip6_hdr);
-	mtu -= sizeof(struct tcphdr);
+    /*  mss = mtu - headers   */
+    mtu -= af == AF_INET ? sizeof(struct iphdr) : sizeof(struct ip6_hdr);
+    mtu -= sizeof(struct tcphdr);
 
-	if(!raw_can_connect()) {	/*  work-around for buggy kernels  */
-	    close (raw_sk);
-	    raw_sk = socket (af, SOCK_RAW, IPPROTO_TCP);
-	    if(raw_sk < 0)  error ("socket");
-	    tune_socket (raw_sk);
-	    /*  but do not connect it...  */
-	}
+    if(!raw_can_connect()) {    /*  work-around for buggy kernels  */
+        close (raw_sk);
+        raw_sk = socket (af, SOCK_RAW, IPPROTO_TCP);
+        if(raw_sk < 0)  error ("socket");
+        tune_socket (raw_sk);
+        /*  but do not connect it...  */
+    }
 
 
-	use_recverr (raw_sk);
+    use_recverr (raw_sk);
 
-	add_poll (raw_sk, POLLIN | POLLERR);
+    add_poll (raw_sk, POLLIN | POLLERR);
 
-	/*  Now create the sample packet.  */
+    /*  Now create the sample packet.  */
 
-	if(!flags)
-	    sysctl = 1;
+    if(!flags)
+        sysctl = 1;
 
-	if(sysctl) {
-	    if(check_sysctl ("ecn"))
-	        flags |= FL_ECN;
-	    
-	    // Forcing TCP SACK, timestamps and Window scale in TCP options. This fix forces the code to generate TCP SYN packets without payload, which eventually would cause the probes to be dropped by regular firewalls. 
+    if(sysctl) {
+        if(check_sysctl ("ecn"))
+            flags |= FL_ECN;
+        
+        // Forcing TCP SACK, timestamps and Window scale in TCP options. This fix forces the code to generate TCP SYN packets without payload, which eventually would cause the probes to be dropped by regular firewalls. 
         // TCP SYN packets with payload are generated due to an initial hard-coded value of 40B of the probe size which was present in the original code and is now used in the new path MTU discovery process. 
         // Please remove this comment once the bug has been fixed.
-	    
-	    flags |= FL_SACK;
-	    flags |= FL_TSTAMP;
-	    flags |= FL_WSCALE;
-	}
+        
+        flags |= FL_SACK;
+        flags |= FL_TSTAMP;
+        flags |= FL_WSCALE;
+    }
 
-	if(!(flags & (FL_FLAGS | 0xff))) {	/*  no any tcp flag set   */
-	    flags |= TH_SYN;
-	    if(flags & FL_ECN)
-		    flags |= TH_ECE | TH_CWR;
-	}
+    if(!(flags & (FL_FLAGS | 0xff))) {    /*  no any tcp flag set   */
+        flags |= TH_SYN;
+        if(flags & FL_ECN)
+            flags |= TH_ECE | TH_CWR;
+    }
 
 
-	/*  For easy checksum computing:
-	    saddr
-	    daddr
-	    length
-	    protocol
-	    tcphdr
-	    tcpoptions
-	*/
+    /*  For easy checksum computing:
+        saddr
+        daddr
+        length
+        protocol
+        tcphdr
+        tcpoptions
+    */
 
-	ptr = tmp_buf;
+    ptr = tmp_buf;
 
-	if(af == AF_INET) {
-	    len = sizeof(src.sin.sin_addr);
-	    memcpy (ptr, &src.sin.sin_addr, len);
-	    ptr += len;
-	    memcpy (ptr, &dest_addr.sin.sin_addr, len);
-	    ptr += len;
-	} else {
-	    len = sizeof(src.sin6.sin6_addr);
-	    memcpy (ptr, &src.sin6.sin6_addr, len);
-	    ptr += len;
-	    memcpy (ptr, &dest_addr.sin6.sin6_addr, len);
-	    ptr += len;
-	}
+    if(af == AF_INET) {
+        len = sizeof(src.sin.sin_addr);
+        memcpy (ptr, &src.sin.sin_addr, len);
+        ptr += len;
+        memcpy (ptr, &dest_addr.sin.sin_addr, len);
+        ptr += len;
+    } else {
+        len = sizeof(src.sin6.sin6_addr);
+        memcpy (ptr, &src.sin6.sin6_addr, len);
+        ptr += len;
+        memcpy (ptr, &dest_addr.sin6.sin6_addr, len);
+        ptr += len;
+    }
 
-	lenp = (uint16_t *) ptr;
-	ptr += sizeof(uint16_t);
-	*((uint16_t *) ptr) = htons ((uint16_t) IPPROTO_TCP);
-	ptr += sizeof(uint16_t);
+    lenp = (uint16_t *) ptr;
+    ptr += sizeof(uint16_t);
+    *((uint16_t *) ptr) = htons ((uint16_t) IPPROTO_TCP);
+    ptr += sizeof(uint16_t);
 
-	/*  Construct TCP header   */
+    /*  Construct TCP header   */
 
-	th = (struct tcphdr *) ptr;
+    th = (struct tcphdr *) ptr;
 
     pseudo_IP_header_size = ptr - tmp_buf;
     
-	th->source = 0;	    /*  temporary   */
-	th->dest = dest_port;
-	th->seq = 0;	    /*  temporary   */
-	th->ack_seq = 0;
-	th->doff = 0;	    /*  later...  */
-	TH_FLAGS(th) = flags & 0xff;
-	th->window = htons (4 * mtu);
-	th->check = 0;
-	th->urg_ptr = 0;
+    th->source = 0;        /*  temporary   */
+    th->dest = dest_port;
+    th->seq = 0;        /*  temporary   */
+    th->ack_seq = 0;
+    th->doff = 0;        /*  later...  */
+    TH_FLAGS(th) = flags & 0xff;
+    th->window = htons (4 * mtu);
+    th->check = 0;
+    th->urg_ptr = 0;
 
 
-	/*  Build TCP options   */
+    /*  Build TCP options   */
 
-	ptr = (uint8_t*)(th + 1);
+    ptr = (uint8_t*)(th + 1);
 
-	if(flags & TH_SYN) {
-	    *ptr++ = TCPOPT_MAXSEG;	/*  2   */
-	    *ptr++ = TCPOLEN_MAXSEG;	/*  4   */
-	    *((uint16_t*) ptr) = htons (mss ? mss : mtu);
-	    ptr += sizeof(uint16_t);
-	}
+    if(flags & TH_SYN) {
+        *ptr++ = TCPOPT_MAXSEG;    /*  2   */
+        *ptr++ = TCPOLEN_MAXSEG;    /*  4   */
+        *((uint16_t*) ptr) = htons (mss ? mss : mtu);
+        ptr += sizeof(uint16_t);
+    }
 
-	if(flags & FL_TSTAMP) {
-	    if(flags & FL_SACK) {
-		    *ptr++ = TCPOPT_SACK_PERMITTED;	/*  4   */
-		    *ptr++ = TCPOLEN_SACK_PERMITTED;/*  2   */
-	    } else {
-		    *ptr++ = TCPOPT_NOP;	/*  1   */
-		    *ptr++ = TCPOPT_NOP;	/*  1   */
-	    }
-	    *ptr++ = TCPOPT_TIMESTAMP;	/*  8   */
-	    *ptr++ = TCPOLEN_TIMESTAMP;	/*  10  */
+    if(flags & FL_TSTAMP) {
+        if(flags & FL_SACK) {
+            *ptr++ = TCPOPT_SACK_PERMITTED;    /*  4   */
+            *ptr++ = TCPOLEN_SACK_PERMITTED;/*  2   */
+        } else {
+            *ptr++ = TCPOPT_NOP;    /*  1   */
+            *ptr++ = TCPOPT_NOP;    /*  1   */
+        }
+        *ptr++ = TCPOPT_TIMESTAMP;    /*  8   */
+        *ptr++ = TCPOLEN_TIMESTAMP;    /*  10  */
 
-	    *((uint32_t*) ptr) = random_seq();	/*  really!  */
-	    ptr += sizeof(uint32_t);
-	    *((uint32_t*) ptr) = (flags & TH_ACK) ? random_seq () : 0;
-	    ptr += sizeof(uint32_t);
-	} else if(flags & FL_SACK) {
-	    *ptr++ = TCPOPT_NOP;	/*  1   */
-	    *ptr++ = TCPOPT_NOP;	/*  1   */
-	    *ptr++ = TCPOPT_SACK_PERMITTED;	/*  4   */
-	    *ptr++ = TCPOLEN_SACK_PERMITTED;	/*  2   */
-	}
+        *((uint32_t*) ptr) = random_seq();    /*  really!  */
+        ptr += sizeof(uint32_t);
+        *((uint32_t*) ptr) = (flags & TH_ACK) ? random_seq () : 0;
+        ptr += sizeof(uint32_t);
+    } else if(flags & FL_SACK) {
+        *ptr++ = TCPOPT_NOP;    /*  1   */
+        *ptr++ = TCPOPT_NOP;    /*  1   */
+        *ptr++ = TCPOPT_SACK_PERMITTED;    /*  4   */
+        *ptr++ = TCPOLEN_SACK_PERMITTED;    /*  2   */
+    }
 
-	if(flags & FL_WSCALE) {
-	    *ptr++ = TCPOPT_NOP;	/*  1   */
-	    *ptr++ = TCPOPT_WINDOW;	/*  3   */
-	    *ptr++ = TCPOLEN_WINDOW;	/*  3   */
-	    *ptr++ = 2;	/*  assume some corect value...  */
-	}
+    if(flags & FL_WSCALE) {
+        *ptr++ = TCPOPT_NOP;    /*  1   */
+        *ptr++ = TCPOPT_WINDOW;    /*  3   */
+        *ptr++ = TCPOLEN_WINDOW;    /*  3   */
+        *ptr++ = 2;    /*  assume some corect value...  */
+    }
 
 
-	len = ptr - (uint8_t*) th;
-	if(len & 0x03)
-	    error("impossible");	/*  as >>2 ...  */
+    len = ptr - (uint8_t*) th;
+    if(len & 0x03)
+        error("impossible");    /*  as >>2 ...  */
 
-	*lenp = htons (len);
-	th->doff = len >> 2;
+    *lenp = htons (len);
+    th->doff = len >> 2;
 
-	length_p = packet_len_p;
+    length_p = packet_len_p;
     if(*length_p && !(buf = malloc(*length_p+pseudo_IP_header_size)))
         error("malloc");
 
@@ -346,65 +346,65 @@ static int tcp_init(const sockaddr_any* dest, unsigned int port_seq, size_t* pac
     
     add_poll(raw_icmp_sk, POLLIN | POLLERR);
     
-	return 0;
+    return 0;
 }
 
 
 static void tcp_send_probe(probe* pb, int ttl)
 {
-	int sk;
-	int af = dest_addr.sa.sa_family;
-	sockaddr_any addr;
-	socklen_t len = sizeof(addr);
+    int sk;
+    int af = dest_addr.sa.sa_family;
+    sockaddr_any addr;
+    socklen_t len = sizeof(addr);
 
-	/*  To make sure we have chosen a free unused "source port",
-	   just create, (auto)bind and hold a socket while the port is needed.
-	*/
+    /*  To make sure we have chosen a free unused "source port",
+       just create, (auto)bind and hold a socket while the port is needed.
+    */
 
-	sk = socket(af, SOCK_STREAM, 0);
-	if(sk < 0)
-	    error ("socket");
+    sk = socket(af, SOCK_STREAM, 0);
+    if(sk < 0)
+        error ("socket");
 
-	if(reuse && setsockopt (sk, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-		error ("setsockopt SO_REUSEADDR");
+    if(reuse && setsockopt (sk, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+        error ("setsockopt SO_REUSEADDR");
 
-	bind_socket(sk);
+    bind_socket(sk);
 
-	if(getsockname (sk, &addr.sa, &len) < 0)
-		error ("getsockname");
+    if(getsockname (sk, &addr.sa, &len) < 0)
+        error ("getsockname");
 
-	/*  When we reach the target host, it can send us either RST or SYN+ACK.
-	  For RST all is OK (we and kernel just answer nothing), but
-	  for SYN+ACK we should reply with our RST.
-	    It is well-known "half-open technique", used by port scanners etc.
-	  This way we do not touch remote applications at all, unlike
-	  the ordinary connect(2) call.
-	    As the port-holding socket neither connect() nor listen(),
-	  it means "no such port yet" for remote ends, and kernel always
-	  send RST in such a situation automatically (we have to do nothing).
-	*/
+    /*  When we reach the target host, it can send us either RST or SYN+ACK.
+      For RST all is OK (we and kernel just answer nothing), but
+      for SYN+ACK we should reply with our RST.
+        It is well-known "half-open technique", used by port scanners etc.
+      This way we do not touch remote applications at all, unlike
+      the ordinary connect(2) call.
+        As the port-holding socket neither connect() nor listen(),
+      it means "no such port yet" for remote ends, and kernel always
+      send RST in such a situation automatically (we have to do nothing).
+    */
 
-	th->source = addr.sin.sin_port;
-	th->seq = random_seq();
-	th->check = 0;
-	th->check = in_csum(buf, (*length_p)+pseudo_IP_header_size);
+    th->source = addr.sin.sin_port;
+    th->seq = random_seq();
+    th->check = 0;
+    th->check = in_csum(buf, (*length_p)+pseudo_IP_header_size);
 
-	if(ttl != last_ttl) {
-	    set_ttl (raw_sk, ttl);
-	    last_ttl = ttl;
-	}
+    if(ttl != last_ttl) {
+        set_ttl (raw_sk, ttl);
+        last_ttl = ttl;
+    }
 
-	pb->send_time = get_time();
+    pb->send_time = get_time();
 
-	if(do_send(raw_sk, th, *length_p, &dest_addr) < 0) {
-	    close (sk);
-	    pb->send_time = 0;
-	    return;
-	}
+    if(do_send(raw_sk, th, *length_p, &dest_addr) < 0) {
+        close (sk);
+        pb->send_time = 0;
+        return;
+    }
 
 
-	pb->seq = th->source;
-	pb->sk = sk;
+    pb->seq = th->source;
+    pb->sk = sk;
 
     // Note that the dest port is incremented for each probe, so we have to specify it explicitly
     // Note also that the source port is changed for each probe (determined by bind) so we have to specify it explicitly too
@@ -413,61 +413,61 @@ static void tcp_send_probe(probe* pb, int ttl)
     memcpy(&pb->src, &src, sizeof(src));
     pb->src.sin.sin_port = addr.sin.sin_port; // valid also for IPv6 as we have a union
     
-	return;
+    return;
 }
 
 
 static probe* tcp_check_reply(int sk, int err, sockaddr_any* from, char* buf, size_t len)
 {
-	probe* pb;
-	struct tcphdr* tcp = (struct tcphdr*) buf;
-	uint16_t sport, dport;
+    probe* pb;
+    struct tcphdr* tcp = (struct tcphdr*) buf;
+    uint16_t sport, dport;
 
-	if(len < 8)
-	    return NULL;	    /*  too short   */
-
-
-	if(err) {
-	    sport = tcp->source;
-	    dport = tcp->dest;
-	} else {
-	    sport = tcp->dest;
-	    dport = tcp->source;
-	}
-
-	if(dport != dest_port)
-		return NULL;
-
-	if(!equal_addr(&dest_addr, from))
-		return NULL;
-
-	pb = probe_by_seq (sport);
-	if(!pb)  return NULL;
+    if(len < 8)
+        return NULL;        /*  too short   */
 
 
-	if(!err) {
-	    pb->final = 1;
+    if(err) {
+        sport = tcp->source;
+        dport = tcp->dest;
+    } else {
+        sport = tcp->dest;
+        dport = tcp->source;
+    }
 
-	    if(info)
-    		pb->ext = names_by_flags(TH_FLAGS(tcp));
-	}
+    if(dport != dest_port)
+        return NULL;
 
-	return pb;
+    if(!equal_addr(&dest_addr, from))
+        return NULL;
+
+    pb = probe_by_seq (sport);
+    if(!pb)  return NULL;
+
+
+    if(!err) {
+        pb->final = 1;
+
+        if(info)
+            pb->ext = names_by_flags(TH_FLAGS(tcp));
+    }
+
+    return pb;
 }
 
 
 static void tcp_recv_probe(int sk, int revents)
 {
-	if(!(revents & (POLLIN | POLLERR)))
-		return;
+    if(!(revents & (POLLIN | POLLERR)))
+        return;
 
-	recv_reply(sk, !!(revents & POLLERR), tcp_check_reply);
+    recv_reply(sk, !!(revents & POLLERR), tcp_check_reply);
 }
 
 
 static void tcp_expire_probe(probe* pb, int* what)
 {
-	probe_done (pb, what);
+    probe_done (pb, what);
 }
 
 static int tcp_is_raw_icmp_sk(int sk)
@@ -497,19 +497,19 @@ static void tcp_handle_raw_icmp_packet(char* bufp)
     probe* pb = probe_by_src_and_dest(&offending_probe_src, &offending_probe_dest);
     
     if(pb) {
-        pb->returned_tos = returned_tos;        
+        pb->returned_tos = returned_tos;
         tcp_expire_probe(pb, &pb->icmp_done);
     }
 }
 
 static tr_module tcp_ops = {
-	.name = "tcp",
-	.init = tcp_init,
-	.send_probe = tcp_send_probe,
-	.recv_probe = tcp_recv_probe,
-	.expire_probe = tcp_expire_probe,
-	.options = tcp_options,
-	.is_raw_icmp_sk = tcp_is_raw_icmp_sk,
+    .name = "tcp",
+    .init = tcp_init,
+    .send_probe = tcp_send_probe,
+    .recv_probe = tcp_recv_probe,
+    .expire_probe = tcp_expire_probe,
+    .options = tcp_options,
+    .is_raw_icmp_sk = tcp_is_raw_icmp_sk,
     .handle_raw_icmp_packet = tcp_handle_raw_icmp_packet
 };
 
