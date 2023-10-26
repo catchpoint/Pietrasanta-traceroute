b649daa9437ddfa160ed2884fdf07fdb62e2680d
lsani <lsani@catchpoint.com>
2023-10-26 15:14:06 +0200

    Alpine support

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -70,7 +70,7 @@ build()
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-for PLATFORM in $(echo "centos7 debian11 ubuntu22")
+for PLATFORM in $(echo "alpine")
 do
     echo "Building for $PLATFORM"
     SAVE_DIR="${SCRIPTPATH}/${PLATFORM}"
