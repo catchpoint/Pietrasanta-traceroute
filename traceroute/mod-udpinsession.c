ff48d21e0e1e6d43787d957d2ef2eec889fbdfe6
lm-luca-sani <luca.sani@logicmonitor.com>
2026-08-03 07:31:22 +0000

    impl

--- traceroute/mod-udpinsession.c
+++ traceroute/mod-udpinsession.c
@@ -1,5 +1,5 @@
 /*
-    Copyright(c)  2023   Alessandro Improta, Luca Sani, Catchpoint Systems, Inc.
+    Copyright(c)  2026   Alessandro Improta, Luca Sani, Catchpoint Systems, Inc.
 
     Copyright(c)  2006, 2007        Dmitry Butskoy
                     <buc@citadel.stu.neva.ru>
